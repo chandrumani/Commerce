5202fb3c6418f40442dcfc69764a5ed55d2f901c
RamachandranM <m.chandru.m@gmail.com>
2021-08-22 03:33:30 +0530

    Server changes and UI

---
+++ server/routers/userRouter.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import data from '../data.js';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
+import { generateToken } from '../utils.js';
+
+const userRouter = express.Router();
+
+userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+})
+);
+
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (user){
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user),
+            });
+            return;
+        }
+    }
+    res.status(401).send({message: 'Invalid user ID or password'});
+}));
+
+userRouter.post('/register', expressAsyncHandler(async(req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.name,8),
+    });
+    const createdUser = await user.save();
+    res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(createdUser),
+    });
+}))
+export default userRouter;
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () =>{
+    dispatch(signout);
+  }
   return (
     <BrowserRouter>
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="/">Commerce</a>
+            <Link className="brand" to="/">Commerce</Link>
         </div>
         <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart
+            {
+              cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )
+            }
+            </Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i>{' '}</Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>Sign out</Link>
+                </ul>
+                </div>
+              ):(
+                <Link to="/signin">Sign In</Link>
+              )
+            }
+
         </div>
     </header>
     <main>
+      <Route path="/cart/:id?" component={CartScreen}></Route>
       <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/signin" component={SigninScreen}></Route>
+      <Route path="/register" component={RegisterScreen}></Route>
       <Route path="/" component={HomeScreen} exact></Route>
         
     </main>
